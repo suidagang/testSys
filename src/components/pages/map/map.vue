012c9b6883b450a098e0048f6eefc051de561a18
suigang <244153653@qq.com>
2018-05-22 10:04:57 +0800

    运用vue-amap引入高德地图 做了一些简单的配置

--- src/components/pages/map/map.vue
+++ src/components/pages/map/map.vue
@@ -5,7 +5,7 @@
             </el-amap>
             <div class="toolbar">
                 <el-button type="danger" @click="add()">增加mark点</el-button>
-                <el-button type="danger" @click="clear()">删除mark点</el-button>
+                <el-button type="danger" @click="clear()">删除全部mark点</el-button>
             </div>
         </div>
     </div>
@@ -34,14 +34,14 @@
                 zoom:12,
                 center:[104.065713,30.657637],
                 amapManager,
-                marker:null,
+                infoWindow:null,
                 events: {
                     init(o) {
 //                        let marker = new AMap.Marker({
-//                            position: [104.056873,30.659704],
+//                            position: [104.065833,30.658318],
 //                            icon: new AMap.Icon({
 //                                size: new AMap.Size(30, 41),  //图标大小
-//                                image: "./static/correct_3.png",
+//                                image: "./static/img/correct_3.png",
 //                                imageOffset: new AMap.Pixel(0, 0)
 //                            })
 //                        });
@@ -57,9 +57,6 @@
             //设置地图容器和高度
             var deyeeMap = document.getElementById('amap-page-container');
             deyeeMap.style.height = (clientHeight - 95) + 'px';
-//            setTimeout(function(){
-//                that.add()
-//            },10000)
             that.$nextTick(function(){
                 that.addMarkers();
             })
@@ -75,28 +72,59 @@
 
         },
         methods:{
+            //一次加很多点
             addMarkers() {
                 var that = this;
-                console.log("add");
                 let o = amapManager.getMap();
+                that.infoWindow  = new AMap.InfoWindow({
+                    offset: new AMap.Pixel(1, -30)
+                });
+                //关闭信息窗体
+                function closeInfoWindow() {
+                    o.clearInfoWindow();
+                };
                 for(let i = 0;i<10 ;i++){
-                    that.marker = new AMap.Marker({
+                    var marker = new AMap.Marker({
+                        map: o,
                         position: [104.061594+ (Math.random() - 0.5) * 0.1,30.682442+ (Math.random() - 0.5) * 0.1],
                         icon: new AMap.Icon({
                             size: new AMap.Size(30, 41),  //图标大小
-                            image: "./static/correct_3.png",
+                            image: "./static/img/correct_3.png",
                             imageOffset: new AMap.Pixel(0, 0)
                         })
                     });
-
-                    that.marker.setMap(o);
+                    that.attachSecretMessage(marker,i);
                 }
 
             },
+            //鼠标点击marker弹出自定义的信息窗体
+            attachSecretMessage(marker,num){
+                let that = this;
+                let o = amapManager.getMap();
+                marker.on('click', function (e) {
+                    //弹出层模板
+                    var windowHTML = '<div class="deyee-map-window-bg">'+'测试'+num+'</div>'
+                    that.infoWindow.setContent(windowHTML);
+                    that.infoWindow.open(o, marker.getPosition());
+                });
+                //点击地图其他地方让窗体消失
+                if (o && typeof o.on == "function") {
+                    o.on('click', function () {
+                        that.infoWindow.close(marker.get('map'), marker);
+                    });
+                }
+            },
+            markerClick(e){
+                let that = this;
+                that.infoWindow.setContent(e.target.content);
+                that.infoWindow.open(map, e.target.getPosition());
+            },
+            //点击新增一个点
             add() {
                 var that = this;
                 let o = amapManager.getMap();
-                that.marker = new AMap.Marker({
+                var marker = new AMap.Marker({
+                    map:o,
                     position: [104.061594+ (Math.random() - 0.5) * 0.1,30.682442+ (Math.random() - 0.5) * 0.1],
                     icon: new AMap.Icon({
                         size: new AMap.Size(30, 41),  //图标大小
@@ -104,10 +132,11 @@
                         imageOffset: new AMap.Pixel(0, 0)
                     })
                 });
-                that.marker.setMap(o);
+                that.attachSecretMessage(marker,100);
             },
             clear(){
-                this.marker.setMap(null);
+                let o = amapManager.getMap();
+                o.clearMap();
             }
         }
     }
